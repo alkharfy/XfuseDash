3cb87af82bb39e18d2251253917a1c6bc0191112
Khalifa Alkharfa <kaalifa.alkharf@gmail.com>
2025-10-09 12:34:53 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/clients/add-client-dialog.tsx
+++ src/components/clients/add-client-dialog.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useForm } from "react";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, DialogFooter, DialogClose } from "@/components/ui/dialog";
