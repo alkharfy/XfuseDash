21405de7865cedbc79893f37c344c7679c46d66a
Khalifa Alkharfa <kaalifa.alkharf@gmail.com>
2025-10-09 07:32:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9132074694-5b878",
+  "appId": "1:292006944543:web:348d4f35d98e273e869a2a",
+  "apiKey": "AIzaSyDgxq7DBfUjf_4FDOtpw15mjc6WmsKCKKE",
+  "authDomain": "studio-9132074694-5b878.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "292006944543"
+};
